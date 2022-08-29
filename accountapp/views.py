730d34cfc137b369ee03efb9adc7a764891c0fc5
cheolseong-son <github-email>
2022-08-29 19:11:27 +0900

    font,divide header, footer, main area

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse("hello world!, 안녕하세요")
+
+    return render(request, 'accountapp/hello_world.html')
