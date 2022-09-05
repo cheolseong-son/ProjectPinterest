657c6d508ff6831dd74c43bb3c4399e1495ded47
cheolseong-son <github-email>
2022-09-05 21:36:07 +0900

    commentapp implementation

---
+++ commentapp/forms.py
@@ -0,0 +1,7 @@
+from commentapp.models import Comment
+from django.forms import ModelForm
+
+class CommentCreationForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
